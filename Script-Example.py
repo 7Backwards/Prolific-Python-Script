81000789d9e06a50cb8181ab0aa00f3c4cc4af8f
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2020-10-01 22:33:05 +0100

    Added retry loop on paused studies

--- Script-Example.py
+++ Script-Example.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-lastStudyID = ""
 while(True):
     try:
         time.sleep(10) # Refreshes every 10s, i recommend not going any lower as there might be security methods implemented
@@ -25,21 +24,21 @@ while(True):
     if len(results) > 0:
 
         study_id = results[0]['id']
+        total_available_places = results[0]['total_available_places']
+        places_taken = results[0]['places_taken']
+        description = results[0]['description']
 
-        if study_id != lastStudyID:
-            
-            print(study_id)
-            url = "https://app.prolific.co/studies/" + study_id 
-            print(url)
-            
-            options = webdriver.ChromeOptions()
-            options.add_argument("user-data-dir=") # Google Chrome User Data Folder
-            #options.add_argument('-headless') # Comment if you want to see chrome appearing
-            driver = webdriver.Chrome(options=options)
-            driver.get(url)
-            wait = WebDriverWait(driver, 10)
+        url = "https://app.prolific.co/studies/" + study_id 
+        print(url)
+        while (True):
             try:
-            
+                options = webdriver.ChromeOptions()
+                options.add_argument("user-data-dir=") # Google Chrome User Data Folder
+                #options.add_argument('-headless') # Comment if you want to see chrome appearing
+                driver = webdriver.Chrome(options=options)
+                driver.get(url)
+                wait = WebDriverWait(driver, 10)
+                
                 search = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'el-button--primary')))
                 search.click()
                 search1 = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'el-button--primary')))
@@ -47,7 +46,6 @@ while(True):
 
                 if search != search1:
                     
-                    lastStudyID = study_id
                     print("Sending email")
                     fromaddr = '' # From Email Address
                     toaddrs  = '' # To Email Address
@@ -61,8 +59,14 @@ while(True):
                     server.sendmail(fromaddr, toaddrs, msg)
                     server.quit()
                     print("Email sent")
-                
+                    break
+                else:
+                    
+                    alertText = driver.find_element_by_class_name('el-notification__content').text
+                    if alertText == "The study is no longer active. We apologise for the inconvenience. Your account is in good standing.":
+                        driver.close()
+                        break
             except :
                 print("Error")
-                continue
-        driver.close()
+                
+            driver.close()
